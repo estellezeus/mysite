a53ec74612fb209354bbe7ac8fcbf2f7cf8be8aa
Estelle Z <zeukengestelle@gmail.com>
2023-08-15 14:28:36 +0100

    asset backend

--- resources/views/layouts/Backend/app.blade.php
+++ resources/views/layouts/Backend/app.blade.php
@@ -8,28 +8,28 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{asset('public/backend/assets/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/plugins/fontawesome-free/css/all.min.css')}}">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{asset('public/backend/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
     <!-- SweetAlert2 -->
-    <link rel="stylesheet" href="{{asset('public/backend/assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
     <!-- Toastr -->
-    <link rel="stylesheet" href="{{asset('public/backend/assets/plugins/toastr/toastr.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/plugins/toastr/toastr.min.css')}}">
     <!-- DataTables -->
-    <link rel="stylesheet" href="{{asset('public/backend/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('public/backend/assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{asset('public/backend/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('public/backend/assets/dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/dist/css/adminlte.min.css')}}">
     <!-- summernote -->
-    <link rel="stylesheet" href="{{asset('public/backend/assets/plugins/summernote/summernote-bs4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/plugins/summernote/summernote-bs4.min.css')}}">
 </head>
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__wobble" src="{{asset('public/backend/assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+        <img class="animation__wobble" src="{{asset('backend/assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
     </div>
 
     <!-- Navbar -->
@@ -59,48 +59,48 @@
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
-<script src="{{asset('public/backend/assets/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap -->
-<script src="{{asset('public/backend/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- overlayScrollbars -->
-<script src="{{asset('public/backend/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- SweetAlert2 -->
-<script src="{{asset('public/backend/assets/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
 <!-- Toastr -->
-<script src="{{asset('public/backend/assets/plugins/toastr/toastr.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/toastr/toastr.min.js')}}"></script>
 <!-- DataTables  & Plugins -->
-<script src="{{asset('public/backend/assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/jszip/jszip.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/pdfmake/pdfmake.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/pdfmake/vfs_fonts.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('public/backend/assets/dist/js/adminlte.js')}}"></script>
+<script src="{{asset('backend/assets/dist/js/adminlte.js')}}"></script>
 
 <!-- PAGE PLUGINS -->
 <!-- jQuery Mapael -->
-<script src="{{asset('public/backend/assets/plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/raphael/raphael.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
-<script src="{{asset('public/backend/assets/plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/raphael/raphael.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
 <!-- ChartJS -->
-<script src="{{asset('public/backend/assets/plugins/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/chart.js/Chart.min.js')}}"></script>
 
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('public/backend/assets/dist/js/demo.js')}}"></script>
+<script src="{{asset('backend/assets/dist/js/demo.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('public/backend/assets/dist/js/pages/dashboard2.js')}}"></script>
+<script src="{{asset('backend/assets/dist/js/pages/dashboard2.js')}}"></script>
 <!-- Summernote -->
-<script src="{{asset('public/backend/assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
 <!-- bs-custom-file-input -->
-<script src="{{asset('public/backend/assets/plugins/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
+<script src="{{asset('backend/assets/plugins/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
 
 @yield('js')
 </body>
